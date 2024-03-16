59b2f270a95b37b1b732db253db7dc69046cfcfa
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2024-03-16 09:49:24 +0200

    change

--- app/management/scripts/bf.py
+++ app/management/scripts/bf.py
@@ -5,13 +5,12 @@ import time
 
 def fetch_listings(base_url, start_page=1):
     page = start_page
-    unique_links = set()  # Множество для хранения уникальных ссылок
+    unique_links = set()
 
     while True:
         url = f'{base_url}?page={page}#items'
         print(f"Обрабатывается: {url}")
 
-        # Создание заголовков для имитации поведения браузера
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
             'Accept-Language': 'en-US,en;q=0.5',
@@ -19,6 +18,7 @@ def fetch_listings(base_url, start_page=1):
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             'Connection': 'keep-alive',
             'Cache-Control': 'max-age=0',
+            'referer': 'https://www.etsy.com'
         }
 
         response = requests.get(url, headers=headers)
@@ -27,14 +27,12 @@ def fetch_listings(base_url, start_page=1):
             break
 
         html = response.content
-        time.sleep(3)  # Задержка в 3 секунды между запросами
+        time.sleep(3)
         soup = BeautifulSoup(html, 'html.parser')
 
-        # Извлечение ссылок на товары
         listings = soup.find_all('a', href=True)
         page_links = [link['href'].split('?')[0] for link in listings if '/listing/' in link['href']]
 
-        # Поиск новых уникальных ссылок
         new_links = set(page_links) - unique_links
         if not new_links:
             print("Новые уникальные товары не найдены на странице. Завершение работы.")
@@ -43,13 +41,7 @@ def fetch_listings(base_url, start_page=1):
         unique_links.update(new_links)
         page += 1
 
-    # Сохранение уникальных ссылок в JSON-файл
     with open('unique_links.json', 'w') as json_file:
         json.dump(list(unique_links), json_file, indent=4)
 
     print(f"\nВсего найдено уникальных ссылок: {len(unique_links)}. Сохранено в unique_links.json")
-
-
-# Базовый URL магазина без параметра страницы
-base_url = 'https://www.etsy.com/shop/BirdTeesUS'
-fetch_listings(base_url)
--- app/management/scripts/data_from_link.py
+++ app/management/scripts/data_from_link.py
@@ -10,6 +10,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
 user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
@@ -24,31 +26,43 @@ user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 ]
+
+
 def get_random_user_agent():
     return random.choice(user_agents)
+
 
 def load_proxies(file_path):
     with open(file_path, 'r') as f:
         proxies = f.read().splitlines()
     return proxies
 
+
 def load_cookies_from_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         cookies = json.load(file)
     return cookies
 
+
 def set_cookies(driver, cookies):
     for cookie in cookies:
         driver.add_cookie(cookie)
+
 
 def fetch_and_parse(url, cookies, proxies):
     results = {'url': url, 'sizes': [], 'colors': []}
 
     random_user_agent = get_random_user_agent()
 
-    # Используем случайный заголовок
+    # Use random user agent
     chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--lang=en-US")
+    chrome_options.add_argument("--disable-cache")
+    chrome_options.add_argument("--disable-web-security")
+    chrome_options.add_argument("--referer=https://www.etsy.com/shop/BirdTeesUS")
     chrome_options.add_argument(f"user-agent={random_user_agent}")
+
     while proxies:
         proxy = proxies.pop(0)
         print(f"Using proxy: {proxy}")
@@ -56,10 +70,10 @@ def fetch_and_parse(url, cookies, proxies):
         # Fetch data and parse
         with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options) as driver:
             driver.get(url)
-            driver.set_page_load_timeout(5)  # Set a timeout for page loading
+            driver.set_page_load_timeout(3)  # Set a timeout for page loading
 
             try:
-                product_name_element = WebDriverWait(driver, 10).until(
+                product_name_element = WebDriverWait(driver, 5).until(
                     EC.presence_of_element_located((By.XPATH, '//*[@id="listing-page-cart"]/div[4]/h1'))
                 )
                 name = product_name_element.text.strip()
@@ -71,8 +85,9 @@ def fetch_and_parse(url, cookies, proxies):
                 results['name'] = "Name not found"
 
             try:
-                item_detail_element = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH, '//*[@id="wt-content-toggle-product-details-read-more"]/p'))
+                item_detail_element = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, '//*[@id="wt-content-toggle-product-details-read-more"]/p'))
                 )
                 item_detail = item_detail_element.text.strip()
                 results['item_detail'] = item_detail
@@ -80,7 +95,7 @@ def fetch_and_parse(url, cookies, proxies):
                 results['item_detail'] = "Item detail not found"
 
             try:
-                color_options = WebDriverWait(driver, 10).until(
+                color_options = WebDriverWait(driver, 5).until(
                     EC.presence_of_element_located((By.XPATH, '//*[@id="variation-selector-1"]'))
                 )
                 sizes_text = color_options.text.strip()
@@ -92,7 +107,7 @@ def fetch_and_parse(url, cookies, proxies):
                 results['sizes'] = "Sizes not found"
 
             try:
-                color_options = WebDriverWait(driver, 10).until(
+                color_options = WebDriverWait(driver, 5).until(
                     EC.presence_of_element_located((By.XPATH, '//*[@id="variation-selector-0"]'))
                 )
                 color_text = color_options.text.strip()
@@ -104,7 +119,14 @@ def fetch_and_parse(url, cookies, proxies):
                 results['colors'] = "Colors not found"
 
             # If request succeeds, break out of the loop
+            print(f"Successfully fetched data for URL: {url}")  # Added print
             break
+
+    # Saving result to file
+    output_file = "output.json"
+    with open(output_file, 'a') as f:
+        json.dump(results, f, indent=4)
+        f.write('\n')
 
     return results
 
@@ -114,9 +136,21 @@ def read_links_from_file(file_path):
         data = json.load(file)
         return data
 
-def main():
+
+async def fetch_multiple(urls, cookies, proxies):
+    loop = asyncio.get_event_loop()
+    with ThreadPoolExecutor(max_workers=15) as executor:
+        tasks = [
+            loop.run_in_executor(executor, fetch_and_parse, url, cookies, proxies)
+            for url in urls
+        ]
+        results = await asyncio.gather(*tasks)
+
+    return results
+
+
+async def main_async():
     links_file = "unique_links.json"
-    output_file = "output.json"
     cookies_file = "cookies.json"
     proxies_file = "proxies.txt"
 
@@ -133,15 +167,8 @@ def main():
     proxies = load_proxies(proxies_file)
 
     links = read_links_from_file(links_file)
-    output_data = []
+    await fetch_multiple(links, cookies, proxies)
 
-    for url in links:
-        print(f"Processing: {url}")
-        result = fetch_and_parse(url, cookies, proxies)
-        output_data.append(result)
-
-    with open(output_file, 'w') as f:
-        json.dump(output_data, f, indent=4)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main_async())
