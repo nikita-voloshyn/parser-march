0b63f9d3970b1923e635d5e41b57f9fcf48c1856
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2024-03-21 08:33:16 +0200

    selenium

--- app/management/scripts/data_from_link.py
+++ app/management/scripts/data_from_link.py
@@ -14,7 +14,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Список user-agent'ов
 user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
@@ -29,30 +28,25 @@ user_agents = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 ]
 
-# Функция для получения случайного user-agent'а
 def get_random_user_agent():
     return random.choice(user_agents)
 
-# Функция для загрузки списка прокси из файла
 def load_proxies(file_path):
     with open(file_path, 'r') as f:
         proxies = f.read().splitlines()
     return proxies
 
-# Функция для загрузки cookies из JSON файла
 def load_cookies_from_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         cookies = json.load(file)
     return cookies
 
-# Функция для установки cookies в браузер
 def set_cookies(driver, cookies):
     for cookie in cookies:
         driver.add_cookie(cookie)
 
-# Функция для загрузки и парсинга данных
 def fetch_and_parse(url, cookies, proxies):
-    results = {'url': url, 'sizes': [], 'price': 0, 'name': '', 'item_detail': ''}
+    results = {'url': url, 'sizes': [], 'price': 0, 'name': '', 'item_detail': '', 'color': ''}
 
     random_user_agent = get_random_user_agent()
 
@@ -72,7 +66,7 @@ def fetch_and_parse(url, cookies, proxies):
         # Fetch data and parse
         with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options) as driver:
             driver.get(url)
-            driver.set_page_load_timeout(5)  # Set a timeout for page loading
+            driver.set_page_load_timeout(5)
 
             try:
                 product_name_element = WebDriverWait(driver, 5).until(
@@ -101,10 +95,47 @@ def fetch_and_parse(url, cookies, proxies):
             except:
                 results['price'] = "Price not found"
 
+            try:
+                color_element = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="product-content"]/div[2]/div/ul/li[2]/div[1]/span/span[2]'))
+                )
+                color = color_element.text.strip()
+                results['color'] = color
+            except:
+                results['color'] = "Color not found"
+
+            try:
+                international_shipment_element = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="product-content"]/div[1]/span[2]/div/div'))
+                )
+                international_shipment = international_shipment_element.text.strip()
+                if international_shipment == "OOPS, THIS ITEM IS CURRENTLY UNAVAILABLE FOR INTERNATIONAL SHIPMENT":
+                    results['international_shipment'] = False
+                else:
+                    results['international_shipment'] = True
+            except:
+                results['international_shipment'] = "International shipment not found"
 
 
-            print(f"Successfully fetched data for URL: {url}")  # Added print
-            break
+            try:
+                item_detail_element = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="pdpMain"]/div[2]/div[5]/div/div/div/div/ul'))
+                )
+                item_detail = item_detail_element.text.strip()
+                results['item_detail'] = item_detail
+            except:
+                results['item_detail'] = "Item detail not found"
+
+            # try:
+            #     url_on_image_element = WebDriverWait(driver, 5).until(
+            #         EC.presence_of_element_located((By.XPATH, '//*[@id="thumbnails"]/div/div/div'))
+            #     )
+            #     url_on_image = url_on_image_element.get_attribute('href ')
+            #     results['url_on_image'] = url_on_image
+            # except:
+            #     results['url_on_image'] = "Url on image not found"
+
+
     # Saving result to file
     output_file = "output.json"
     with open(output_file, 'a') as f:
@@ -121,7 +152,7 @@ def read_links_from_file(file_path):
 
 
 async def fetch_multiple(urls, cookies, proxies):
-    results = []  # Создаем список результатов
+    results = []
 
     loop = asyncio.get_event_loop()
     with ThreadPoolExecutor(max_workers=15) as executor:
@@ -130,7 +161,7 @@ async def fetch_multiple(urls, cookies, proxies):
             for url in urls
         ]
         for result in await asyncio.gather(*tasks):
-            results.append(result)  # Добавляем результаты в список
+            results.append(result)
 
     return results
 
